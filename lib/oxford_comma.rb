b4ba3f3ff48007686aada3021a5b784f5b5819fb
Adam Gordon <adamjgordon24@gmail.com>
2019-01-27 22:57:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
    #code
   elsif array.length > 3
   lasty = array.pop
-  sentence = array.join(",")
+  sentence = array.join(", ")
   sentence.concat(" and #{lasty}")
   else 
     array.join(", ")

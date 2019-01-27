937ddf94d855263ad2a5f597f84023ce389e2d11
Adam Gordon <adamjgordon24@gmail.com>
2019-01-27 22:50:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
+  elsif array.length == 3
+   #code
+  elsif array.length > 3
+  #code
   else 
     array.join(", ")
   end
